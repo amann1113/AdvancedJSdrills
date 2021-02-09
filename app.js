7ca5c2e0a8a962f3a0026ba453728b3e0dd2fe9d
Austin Mann <austin.mann1113@gmail.comgit config --global user.name Austin>
2021-02-09 13:45:09 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// name = 'Austin';
+// var name;
+// console.log(name);
+
+
+// setName();
+// function setName() {
+//     var name = 'Austin';
+//     console.log(name);
+// }
+
+// console.log('some text');
+// let avg = findAvg(2, 2);
+// console.log('some text', avg);
+// function findAvg(a, b) {
+//     console.log('some text');
+//     var answer = ( a + b) / 2;
+//     return answer;
+// }
+
+// let fruits = ['Apple', 'Orange', 'Kiwi'];
+// // let favFruit 
+
+// function printFruits(){
+//     favFruit = fruits[2];
+//     console.log(fruits[0]);
+// }
+
+// function printFavFruit() {
+//     console.log(favFruit);
+// }
+
+// printFruits();
+// printFavFruit();
+
+helloFunc();
+function helloFunc(){
+    let name = "Austin";
+    console.log("Hello " + name);
+}
+
+var newFunc = function(){
+    alert("Something should pop up");
+}
+newFunc();
+
+
+
+
+
+
+
